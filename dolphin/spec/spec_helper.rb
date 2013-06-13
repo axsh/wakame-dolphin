3d1c00de13acef5db2a3b269aabf32d786287c9b
saicologic <s-mikami@axsh.net>
2013-06-13 23:52:52 +0900

    Load bundle setup

--- dolphin/spec/spec_helper.rb
+++ dolphin/spec/spec_helper.rb
@@ -4,6 +4,9 @@ require 'rubygems'
 require 'rspec'
 require File.join(File.expand_path('../../', __FILE__), 'lib/dolphin')
 
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path(File.dirname(__FILE__) + '/Gemfile')
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+Bundler.require :default if defined?(Bundler)
 
 # TODO: Factory Girl
 # TODO: Rack Test
