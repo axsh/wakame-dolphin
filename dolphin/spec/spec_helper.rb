1a5161fc29e30c529b24c743a2bf61d1c5f17fe3
saicologic <s-mikami@axsh.net>
2013-06-17 00:20:21 +0900

    Load helper files

--- dolphin/spec/spec_helper.rb
+++ dolphin/spec/spec_helper.rb
@@ -8,6 +8,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path(File.dirname(__FILE__) + '/Gemfile')
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 Bundler.require :default if defined?(Bundler)
 
+# Load helper files
+Dir.glob('./spec/helpers/*_helper.rb').each {|f| require File.expand_path(f) }
 
 RSpec.configure do |c|
   c.filter_run_excluding :smtp => true
