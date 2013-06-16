a48cedbce4d4a91b2d2792c52449432ca92c93c5
saicologic <s-mikami@axsh.net>
2013-06-17 00:19:51 +0900

    Delete TODO

--- dolphin/spec/spec_helper.rb
+++ dolphin/spec/spec_helper.rb
@@ -8,9 +8,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path(File.dirname(__FILE__) + '/Gemfile')
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 Bundler.require :default if defined?(Bundler)
 
-# TODO: Factory Girl
-# TODO: Rack Test
-# TODO: JSON Test
+
 RSpec.configure do |c|
   c.filter_run_excluding :smtp => true
 end
