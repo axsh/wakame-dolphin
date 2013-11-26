bac5942a638e5925b53a9849f37d807a3d06178d
saicologic <s-mikami@axsh.net>
2013-11-26 17:03:49 +0900

    Add test load external template

--- spec/files/message_builder_spec.rb
+++ spec/files/message_builder_spec.rb
@@ -23,5 +23,32 @@ describe Dolphin::TemplateBuilder do
 
       expect(template).to be_nil
     end
+
+    it 'expect to load external template' do
+      message_type = 'test'
+
+      expect(Dolphin.settings['template']).to be_true
+      expect(Dolphin.settings['template']['template_path']).to be_true
+
+      mb = Dolphin::MessageBuilder::Mail.new
+      bind_params = {
+        'messages' => {
+          'subject' => 'subject',
+          'body' => 'body'
+        },
+        "to" => "to@example.com",
+        "cc" => "cc@example.com",
+        "bcc" =>"bcc@example.com"
+      }
+      notification_object = mb.build(message_type, bind_params)
+
+      expect(notification_object).not_to be_nil
+      expect(notification_object).to be_kind_of Dolphin::NotificationObject
+      expect(notification_object.subject).to eq bind_params['messages']['subject']
+      expect(notification_object.body).to eq bind_params['messages']['body']
+      expect(notification_object.to).to eq bind_params['to']
+      expect(notification_object.cc).to eq bind_params['cc']
+      expect(notification_object.bcc).to eq bind_params['bcc']
+    end
   end
 end
