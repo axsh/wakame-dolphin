441a063e1abc7411ffd9637bbdaa985164d2e81e
saicologic <s-mikami@axsh.net>
2013-12-13 19:27:49 +0900

    Add nil check

--- lib/dolphin/models/cassandra/base.rb
+++ lib/dolphin/models/cassandra/base.rb
@@ -2,9 +2,13 @@
 module Dolphin
   module Models::Cassandra
     class Base
-      attr_accessor :db
       def initialize(connection)
         @db = connection
+      end
+
+      def db
+        raise 'Connection refused' if @db.nil?
+        @db
       end
     end
   end
